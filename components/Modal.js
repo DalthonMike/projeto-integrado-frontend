20a79b5e7c67cd79854a75b7f5569bfe6ea129cc
Marcos <markvini055@gmail.com>
2023-06-26 22:20:41 -0300

    Revert "Update Modal Detalhes Deputados"
    This reverts commit 8342a66b82cd69e61f5b6f159a6c68df38888bde.

--- components/Modal.js
+++ components/Modal.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import styles from '../styles/modal.module.css'
-import { Card } from "react-bootstrap";
 
-const Modal = ({ isOpen, onClose, children, deputado }) => {
+const Modal = ({ isOpen, onClose, children }) => {
 
     if (!isOpen) return null;
 
@@ -17,16 +16,6 @@ const Modal = ({ isOpen, onClose, children, deputado }) => {
                     <a title='Fechar'>x</a>
                 </button>
                 {children}
-                <Card style={{width: '18rem'}}>
-                    <Card.Img variant="top" src={deputado.urlFoto} />
-                    <Card.Body>
-                        <Card.Title>{deputado.nome}</Card.Title>
-                        <Card.Text>
-                            Partido: {deputado.siglaPartido} <br />
-                            Estado: {deputado.siglaUf}
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
                 <button className={styles.modalBtn} onClick={onClose}>
                     <a title='Detalhes do Deputado'>Detalhes</a>
                 </button>
--- service/api.js
+++ service/api.js
@@ -4,22 +4,4 @@ const api = axios.create({
   baseURL: 'https://dadosabertos.camara.leg.br/api/v2',
 });
 
-api.get('/deputados')
-  .then(response => {
-    console.log(response.data); // Aqui estão os dados recebidos
-  })
-  .catch(error => {
-    if (error.response) {
-      // O pedido foi feito e o servidor respondeu com um status fora do intervalo 2xx
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // O pedido foi feito, mas não houve resposta
-      console.log(error.request);
-    } else {
-      // Algo aconteceu na configuração do pedido que disparou um erro
-      console.log('Error', error.message);
-    }
-    console.log(error.config);
-  });
+export default api;
